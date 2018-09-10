bccfacc7acc692951d14b98af25edb4f675b68c2
Grégory Le Garec <gregory.legarec@gmail.com>
2018-09-10 16:47:55 +0200

    feat: Initial commit 🎉

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { BaseKonnector } = require('cozy-konnector-libs')
+
+const konnectorErrors = [
+  'CHALLENGE_ASKED',
+  'LOGIN_FAILED',
+  'LOGIN_FAILED.NEEDS_SECRET',
+  'LOGIN_FAILED.TOO_MANY_ATTEMPTS',
+  'MAINTENANCE',
+  'NOT_EXISTING_DIRECTORY',
+  'USER_ACTION_NEEDED',
+  'USER_ACTION_NEEDED.ACCOUNT_REMOVED',
+  'USER_ACTION_NEEDED.CHANGE_PASSWORD',
+  'USER_ACTION_NEEDED.PERMISSIONS_CHANGED',
+  'VENDOR_DOWN',
+  'VENDOR_DOWN.BANK_DOWN',
+  'VENDOR_DOWN.LINXO_DOWN'
+]
+
+async function start(fields) {
+  const timeout = Number(fields.timeout) || 1000
+  if (timeout > 0) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (
+          !!fields.login &&
+          konnectorErrors.includes(fields.login.toUpperCase())
+        ) {
+          throw new Error(fields.login.toUpperCase())
+        }
+        resolve()
+      }, timeout)
+    })
+  }
+}
+
+module.exports = new BaseKonnector(start)
