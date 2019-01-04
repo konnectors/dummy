b238224fd4210defc4391c29577c530e2305e517
Grégory Le Garec <gregory.legarec@gmail.com>
2019-01-04 18:22:51 +0100

    fix: Hard code repository 😭

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 set -e
 
-DEPLOY_REPOSITORY=$1
-
 # Deploy main konnector
-git-directory-deploy --directory build/ --branch build --repo="$DEPLOY_REPOSITORY",
+git-directory-deploy --directory build/ --branch build --repo="git@github.com:konnectors/dummy.git",
 
 # Deploy all flavous of Dummy
 find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
-  git-directory-deploy --directory "build-$d/" --branch "build_$d" --repo="$DEPLOY_REPOSITORY"
+  git-directory-deploy --directory "build-$d/" --branch "build_$d" --repo="git@github.com:konnectors/dummy.git"
 done
