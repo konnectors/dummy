6c902d871436fea851cb9f22a3a8eea7bfc1371c
Grégory Le Garec <gregory.legarec@gmail.com>
2019-01-21 17:10:31 +0100

    feat: Better manifest merge 📝

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -5,7 +5,7 @@ set -e
 find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
     rm -Rf "build-$d"
     cp -R build "build-$d"
-    jq -s '.[0] * .[1]' "build-$d/manifest.konnector" "flavours/$d/manifest.fragment.json" > "build-$d/manifest.temp"
+    jq -s '.[0] + .[1]' "build-$d/manifest.konnector" "flavours/$d/manifest.fragment.json" > "build-$d/manifest.temp"
     rm "build-$d/manifest.konnector"
     mv "build-$d/manifest.temp" "build-$d/manifest.konnector"
 done
