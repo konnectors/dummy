fd413aa8b80845800909a241389fd29bb784f802
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-07 01:54:50 +0100

    feat: Copy icon in flavour build ✨

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -5,6 +5,9 @@ set -e
 find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
     rm -Rf "build-$d"
     cp -R build "build-$d"
+    # icon
+    cp "flavours/$d/dummy.svg" "build-$d/dummy.svg" 2>/dev/null || :
+    # manifest
     jq -s '.[0] + .[1]' "build-$d/manifest.konnector" "flavours/$d/manifest.fragment.json" > "build-$d/manifest.temp"
     rm "build-$d/manifest.konnector"
     mv "build-$d/manifest.temp" "build-$d/manifest.konnector"
