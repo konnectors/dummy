5597e41d2510c798549ba88ee93aace837d5a8c7
doubleface <christophe@cozycloud.cc>
2019-08-19 17:45:36 +0200

    chore: publish twofa dummy to registry

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,9 +4,10 @@ set -e
 REGISTRY_TOKEN=$1
 
 # Publish main konnector
-cozy-app-publish --token $REGISTRY_TOKEN --build-url "https://github.com/konnectors/dummy.git#build"
+# cozy-app-publish --token $REGISTRY_TOKEN --build-url "https://github.com/konnectors/dummy.git#build"
+cozy-app-publish --token $REGISTRY_TOKEN --build-dir "./build-twofa" --build-url "https://github.com/konnectors/dummy.git#build_twofa"
 
 # Publish all flavours of Dummy
-find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
-  cozy-app-publish --token $REGISTRY_TOKEN --build-dir "./build-$d" --build-url "https://github.com/konnectors/dummy.git#build_$d"
-done
+# find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
+#   cozy-app-publish --token $REGISTRY_TOKEN --build-dir "./build-$d" --build-url "https://github.com/konnectors/dummy.git#build_$d"
+# done
