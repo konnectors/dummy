407bace53fe58be3acdc31d3358b692deac74266
doubleface <christophe@cozycloud.cc>
2020-04-02 14:12:02 +0200

    fix: build all flavours correctly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -e
 scripts/postbuild.sh
 yarn git-directory-deploy --directory build/ --branch build --repo="$DEPLOY_REPOSITORY"
 
-# Deploy all flavous of Dummy
+# Deploy all flavours of Dummy
 find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
   yarn git-directory-deploy --directory "build-$d/" --branch "build_$d" --repo="$DEPLOY_REPOSITORY"
 done
