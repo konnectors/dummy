acd4ef65b1fde668ff70efe5cb066982134b931f
LucsT <lucas@cozycloud.cc>
2021-03-22 21:34:45 +0100

    fix: Add default repo in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,10 @@ set -e
 
 # Deploy main konnector
 scripts/postbuild.sh
-yarn git-directory-deploy --directory build/ --branch build --repo="$DEPLOY_REPOSITORY"
+yarn git-directory-deploy --directory build/ --branch build --repo=${DEPLOY_REPOSITORY:-$npm_package_repository_url}
 
 # Deploy all flavours of Dummy
 find flavours/* -prune -type d -exec basename {} \; | while IFS= read -r d; do
-  yarn git-directory-deploy --directory "build-$d/" --branch "build_$d" --repo="$DEPLOY_REPOSITORY"
+  yarn git-directory-deploy --directory "build-$d/" --branch "build_$d" --repo=${DEPLOY_REPOSITORY:-$npm_package_repository_url}
+
 done
